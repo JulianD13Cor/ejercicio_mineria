0b9d17a3cb8f18e17abddd548383243946003eaf
zip-b205-e-020 <zip-b205-e-020@UMD.LOCAL.EDU>
2023-02-10 19:28:44 -0500

    tercer commit

--- src/principal/principal.java
+++ src/principal/principal.java
@@ -3,5 +3,7 @@ package principal;
 public class principal {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("hola2");
+        System.out.println("hola3");
     }
 }
